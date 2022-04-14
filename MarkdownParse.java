ca1295fdee69c5733a65508d943efdd137e1f303
mwchen25 <mwchen@ucsd.edu>
2022-04-14 12:36:46 -0700

    Fix infinite loop bug
    Fix bug where infinite loop was triggered by file reader overlapping to start of file

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,13 +11,15 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
+        // System.out.println("FILE LENGTH: " + markdown.length());
+        while(currentIndex < markdown.length() && markdown.indexOf("[", currentIndex) >= currentIndex) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            // System.out.println("CURRENT INDEX: " + currentIndex);
         }
 
         return toReturn;
